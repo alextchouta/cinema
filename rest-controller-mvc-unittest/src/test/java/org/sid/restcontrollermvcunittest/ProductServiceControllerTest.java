b6a31f365a9765591e64e69721fb5213143fbc4f
alextchouta <alextchouta@yahoo.fr>
2020-09-10 16:03:39 +0200

    implements controller

--- rest-controller-mvc-unittest/src/test/java/org/sid/restcontrollermvcunittest/ProductServiceControllerTest.java
+++ rest-controller-mvc-unittest/src/test/java/org/sid/restcontrollermvcunittest/ProductServiceControllerTest.java
@@ -1,4 +1,83 @@
 package org.sid.restcontrollermvcunittest;
 
-public class ProductServiceControllerTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.sid.restcontrollermvcunittest.controller.Product;
+
+import org.springframework.http.MediaType;
+
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ProductServiceControllerTest extends AbstractTest {
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    public void getProductsList() throws Exception {
+        String uri = "/products";
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                                .accept("application/json")
+                                .contentType("application/json"))
+                                .andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+
+        String content = mvcResult.getResponse().getContentAsString();
+        Product[] productList = super.mapFromJson(content, Product[].class);
+
+        assertTrue(productList.length > 0);
+    }
+    @Test
+    public void createProduct() throws Exception {
+        String uri = "/products";
+        Product product = new Product();
+        product.setId("3");
+        product.setName("Ginger");
+        String inputJson = super.mapToJson(product);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(201, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "Product is created successfully");
+    }
+
+    @Test
+    public void updateProduct() throws Exception {
+        String uri = "/products/2";
+        Product product = new Product();
+        product.setName("Lemon");
+        String inputJson = super.mapToJson(product);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri)
+                .contentType("application/json")
+                .content("application/json")
+                .content(inputJson)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "Product is updated successsfully");
+    }
+
+    @Test
+    public void deleteProduct() throws Exception {
+        String uri = "/products/2";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "Product is deleted successsfully");
+    }
 }
