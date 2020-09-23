9b6eabdb4d45875743b6428cfd6571611c59bb0b
alextchouta <alextchouta@yahoo.fr>
2020-09-23 14:51:12 +0200

    Openclassroom Tester le code java (E2E, IT, Unit Test) avec selenium, cucumber

---
+++ oc-testing-java-cours-master/src/test/java/com/openclassrooms/testing/calcul/e2e/StudentMultiplicationJourneyE2E.java
@@ -0,0 +1,93 @@
+package com.openclassrooms.testing.calcul.e2e;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StudentMultiplicationJourneyE2E {
+
+	@LocalServerPort
+	private Integer port;
+	private WebDriver webDriver = null;
+	private String baseUrl;
+
+	@BeforeAll
+	public static void setUpFirefoxDriver() {
+		WebDriverManager.firefoxdriver().setup();
+	}
+
+	@BeforeEach
+	public void setUpWebDriver() {
+		webDriver = new FirefoxDriver();
+		baseUrl = "http://localhost:" + port + "/calculator";
+	}
+
+	@AfterEach
+	public void quitWebDriver() {
+		if (webDriver != null) {
+			webDriver.quit();
+		}
+	}
+
+	@Test
+	public void aStudentUsesTheCalculatorToMultiplyTwoBySixteen() {
+
+		// GIVEN
+		webDriver.get(baseUrl);
+		final WebElement leftField = webDriver.findElement(By.id("left"));
+		final WebElement typeDropdown = webDriver.findElement(By.id("type"));
+		final WebElement rightField = webDriver.findElement(By.id("right"));
+		final WebElement submitButton = webDriver.findElement(By.id("submit"));
+
+		// WHEN
+		leftField.sendKeys("2");
+/*		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+		typeDropdown.sendKeys("x");
+/*		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+		rightField.sendKeys("16");
+/*		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+		submitButton.click();
+
+/*		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+
+		// THEN
+		final WebDriverWait waiter = new WebDriverWait(webDriver, 5);
+		final WebElement solutionElement = waiter.until(
+				ExpectedConditions.presenceOfElementLocated(By.id("solution")));
+		final String solution = solutionElement.getText();
+		assertThat(solution).isEqualTo("32"); // 2 x 16
+	}
+}
